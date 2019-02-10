a59b10351e083b7de9ae1ab6f6e6835b6f49fb7b
Ikanant <jonathan.hdez92@gmail.com>
2019-02-10 16:49:59 -0500

    use Autowire byName

--- spring_sample_xml/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_xml/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -7,19 +7,22 @@ import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 public class CustomerServiceImpl implements CustomerService {
-	
-	private CustomerRepository customerRepository; 
-	
+
+	private CustomerRepository customerRepository;
+
+	public CustomerServiceImpl() {
+	}
+
 	public CustomerServiceImpl(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
-	
+
 	@Override
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
 	}
 
-	public void setCustomerRepository(CustomerRepository customerRepository) {
+	public void setFoo(CustomerRepository customerRepository) {
 		this.customerRepository = customerRepository;
 	}
 }
